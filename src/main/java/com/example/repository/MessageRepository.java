ca597cc9782dd835bbac913e181a9f54eea8fccc
Alec Figueroa <aleclfig@yahoo.com>
2025-04-08 09:14:28 +0000

    Setting Annotations

--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -1,8 +1,10 @@
 package com.example.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.entity.Message;
 
+@Repository
 public interface MessageRepository extends JpaRepository<Message, Integer>{
 }
--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,15 @@
 package com.example.service;
 
+import com.example.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class AccountService {
+
+    AccountRepository accountRepository;
+
+    @Autowired
+    public AccountService(AccountRepository accountRepository)
+    {
+        this.accountRepository = accountRepository;
+    }
 }
